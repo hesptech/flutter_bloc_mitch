bd5fc01dea4a0c1f461cc181f6232c60b37e047c
hesptech <olea.oswaldo@gmail.com>
2024-10-21 17:07:45 +0200

    init git

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_bloc_mitch
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_mitch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_mitch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
